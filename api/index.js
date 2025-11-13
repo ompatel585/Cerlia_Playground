5c95ddf14ecb511fe20a132fd50d96b090cf9aed
Om Patel <84760165+ompatel585@users.noreply.github.com>
2025-11-13 21:23:43 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,8 +3,6 @@ import serverless from "serverless-http";
 import app from "../server/server.js";
 import connectDB from "../server/config/db.js";
 
-// Connect to Mongo once at cold start
 await connectDB();
 
-// Export wrapped Express app
 export default serverless(app);
