4fbd863e69131f3f6228c86406c66873c12c6473
Om Patel <84760165+ompatel585@users.noreply.github.com>
2025-11-13 22:10:01 +0530

    Refactor DB connection and export handler function
    Refactor database connection handling and simplify the export of the handler function.

--- api/index.js
+++ api/index.js
@@ -1,21 +1,16 @@
 // api/index.js
-// api/index.js
 import serverless from "serverless-http";
 import app from "../server/server.js";
 import connectDB from "../server/config/db.js";
 
-// Connect to Mongo ONLY on cold start
-let isConnected = false;
+// Initialize DB connection once
+let handler = null;
 
-async function init() {
-  if (!isConnected) {
+export default async function (req, res) {
+  if (!handler) {
     await connectDB();
-    isConnected = true;
+    handler = serverless(app);
   }
-}
-
-export default async function handler(req, res) {
-  await init();
-  const expressHandler = serverless(app);
-  return expressHandler(req, res);
+  
+  return handler(req, res);
 }
