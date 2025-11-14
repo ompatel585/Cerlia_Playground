c4a805adde8593aacc40d54a3d254fa8d1793540
Om Patel <84760165+ompatel585@users.noreply.github.com>
2025-11-14 22:04:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -94,16 +94,22 @@ const allowedOrigins = [
   "http://localhost:5174"
 ];
 
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: true,
+  credentials: true
 }));
+
 
 app.use(bodyParser.json());
 
