18b0aaab5ce242bcdc8fe9f0b44721e95ee25e05
Om Patel <84760165+ompatel585@users.noreply.github.com>
2025-11-14 22:04:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,14 +3,12 @@ import serverless from "serverless-http";
 import app from "../server/server.js";
 import connectDB from "../server/config/db.js";
 
-// Initialize DB connection once
-let handler = null;
+let handler;
 
 export default async function (req, res) {
   if (!handler) {
-    await connectDB();
+    await connectDB();       // connect DB only once
     handler = serverless(app);
   }
-  
   return handler(req, res);
 }
